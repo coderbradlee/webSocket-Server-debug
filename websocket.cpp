0b404468f6006a32aac3a0c22abe3159a7b8c269
lzxm160 <lzxm160@sina.com>
2016-04-26 14:50:56 +0800

    test wss

--- websocket.cpp
+++ websocket.cpp
@@ -73,8 +73,44 @@ int main() {
 			//Start WS-server
 			servers.start();
 		});
-		this_thread::sleep_for(chrono::seconds(1));
+		this_thread::sleep_for(chrono::seconds(2));
 
+
+
+{
+	//test wss
+	WssClient client("localhost:8081/upload", false);
+    client.onmessage=[&client](shared_ptr<WssClient::Message> message) {
+        auto message_str=message->string();
+        
+        cout << "Client: Message received: \"" << message_str << "\"" << endl;
+        
+        cout << "Client: Sending close connection" << endl;
+        client.send_close(1000);
+    };
+    
+    client.onopen=[&client]() {
+        cout << "Client: Opened connection" << endl;
+        
+        string message="Hello";
+        cout << "Client: Sending message: \"" << message << "\"" << endl;
+
+        auto send_stream=make_shared<WssClient::SendStream>();
+        *send_stream << message;
+        client.send(send_stream);
+    };
+    
+    client.onclose=[](int status, const string& reason) {
+        cout << "Client: Closed connection with status code " << status << endl;
+    };
+    
+    //See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+    client.onerror=[](const boost::system::error_code& ec) {
+        cout << "Client: Error: " << ec << ", error message: " << ec.message() << endl;
+    };
+    
+    client.start();
+}
 
 
 		server_threads.join();
